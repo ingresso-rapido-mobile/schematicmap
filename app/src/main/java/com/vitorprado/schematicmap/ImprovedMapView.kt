a66a989d1fe0e16de053480d7215fc2c4eb1155b
Bloder <aptgetinstalldanielleme@gmail.com>
2018-05-10 18:06:41 -0300

    Let user choose about map view window locking

--- app/src/main/java/com/vitorprado/schematicmap/ImprovedMapView.kt
+++ app/src/main/java/com/vitorprado/schematicmap/ImprovedMapView.kt
@@ -11,6 +11,11 @@ private const val TOP = 0
 private const val BOTTOM = 1
 private const val RIGHT = 2
 private const val LEFT = 3
+private const val TOUCH_STATE_NO = 0
+private const val TOUCH_STATE_SCROLL = 1
+private const val TOUCH_STATE_SCALE = 2
+private const val TOUCH_STATE_ROTATE = 3
+private const val TOUCH_STATE_TWO_POINTED = 4
 
 open class ImprovedMapView : MapView {
 
@@ -19,8 +24,9 @@ open class ImprovedMapView : MapView {
     private var maxY: Float = 10000f
     private var minZoomModifier = 0f
     private var maxZoomModifier = 0f
-    private var lastXPosition: Float = 0f
-    private var lastYPosition: Float = 0f
+    private var xPosition: Float = 0f
+    private var yPosition: Float = 0f
+    private var lockImageInWindow = false
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -31,6 +37,10 @@ open class ImprovedMapView : MapView {
         this.maxY = y
     }
 
+    fun shouldLockImageInWindow() {
+        lockImageInWindow = true
+    }
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> downEvent = Pair(event?.x?:0f, event?.y?:0f)
@@ -38,17 +48,71 @@ open class ImprovedMapView : MapView {
         return superClassTouchModified(event!!)
     }
 
+    private fun onMapViewRotate(event: MotionEvent) {
+        val newDegree: Float = this.rotation(event, this.mid)
+        val rotate: Float
+        this.currentMatrix.set(this.saveMatrix)
+        rotate = newDegree - this.oldDegree
+        this.currentRotateDegrees = (rotate + this.saveRotateDegrees) % 360.0f
+        this.currentRotateDegrees = if (this.currentRotateDegrees > 0.0f) this.currentRotateDegrees else this.currentRotateDegrees + 360.0f
+        this.refresh()
+    }
+
+    private fun onZoomMapView(event: MotionEvent) {
+        val newDist: Float
+        var scale: Float
+        this.currentMatrix.set(this.saveMatrix)
+        newDist = this.distance(event, this.mid)
+        scale = newDist / this.oldDist
+        if (lockImageInWindow) setDynamicImageZoom()
+        if (scale * this.saveZoom < this.minZoom) {
+            scale = this.minZoom / this.saveZoom
+        } else if (scale * this.saveZoom > this.maxZoom) {
+            scale = this.maxZoom / this.saveZoom
+        }
+
+        this.currentZoom = scale * this.saveZoom
+        this.currentMatrix.postScale(scale, scale, this.mid.x, this.mid.y)
+        this.refresh()
+    }
+
+    private fun onScrollMapView(event: MotionEvent) {
+        insertNewCoordinates(event)
+        this.currentMatrix.set(this.saveMatrix)
+        this.currentMatrix.postTranslate(xPosition, yPosition)
+        this.refresh()
+    }
+
+    private fun setDynamicImageZoom() = if (minZoomModifier == 0f && maxZoomModifier == 0f) {
+        minZoomModifier = this.saveZoom
+        maxZoomModifier = this.saveZoom + .7f
+        setMinZoom(minZoomModifier)
+        setMaxZoom(maxZoomModifier)
+    } else { null }
+
+
+    private fun insertNewCoordinates(event: MotionEvent) = when(lockImageInWindow) {
+        true -> {
+            xPosition = if (canDragX(event)) event.x - this.startTouch.x else xPosition
+            yPosition = if (canDragY(event)) event.y - this.startTouch.y else yPosition
+        }
+        else -> {
+            xPosition = event.x - this.startTouch.x
+            yPosition = event.y - this.startTouch.y
+        }
+    }
+
     private fun superClassTouchModified(event: MotionEvent): Boolean {
         if (!this.isMapLoadFinish) {
             return false
         } else {
             when (event.action and 255) {
-                0 -> {
+                MotionEvent.ACTION_DOWN -> {
                     this.saveMatrix.set(this.currentMatrix)
                     this.startTouch.set(event.x, event.y)
                     this.currentTouchState = 1
                 }
-                1 -> {
+                MotionEvent.ACTION_UP -> {
                     if (this.withFloorPlan(event.getX(), event.getY())) {
                         val scale1 = this.layers.iterator()
 
@@ -59,65 +123,34 @@ open class ImprovedMapView : MapView {
 
                     this.currentTouchState = 0
                 }
-                2 -> {
-                    val newDist: Float
-                    val newDegree: Float
-                    var scale: Float
-                    val rotate: Float
-
+                MotionEvent.ACTION_MOVE -> {
                     when (this.currentTouchState) {
-                        1 -> {
-                            this.currentMatrix.set(this.saveMatrix)
-                            if (canDragX(event)) lastXPosition = event.x - this.startTouch.x
-                            if (canDragY(event)) lastYPosition = event.y - this.startTouch.y
-                            this.currentMatrix.postTranslate(lastXPosition, lastYPosition)
-                            this.refresh()
+                        TOUCH_STATE_SCROLL -> {
+                            onScrollMapView(event)
                         }
-                        2 -> {
-                            this.currentMatrix.set(this.saveMatrix)
-                            newDist = this.distance(event, this.mid)
-                            scale = newDist / this.oldDist
-                            if (minZoomModifier == 0f && maxZoomModifier == 0f) {
-                                minZoomModifier = this.saveZoom
-                                maxZoomModifier = this.saveZoom + .7f
-                                setMinZoom(minZoomModifier)
-                                setMaxZoom(maxZoomModifier)
-                            }
-                            if (scale * this.saveZoom < this.minZoom) {
-                                scale = this.minZoom / this.saveZoom
-                            } else if (scale * this.saveZoom > this.maxZoom) {
-                                scale = this.maxZoom / this.saveZoom
-                            }
-
-                            this.currentZoom = scale * this.saveZoom
-                            this.currentMatrix.postScale(scale, scale, this.mid.x, this.mid.y)
-                            this.refresh()
+                        TOUCH_STATE_SCALE -> {
+                            onZoomMapView(event)
                         }
-                        3 -> {
-                            this.currentMatrix.set(this.saveMatrix)
-                            newDegree = this.rotation(event, this.mid)
-                            rotate = newDegree - this.oldDegree
-                            this.currentRotateDegrees = (rotate + this.saveRotateDegrees) % 360.0f
-                            this.currentRotateDegrees = if (this.currentRotateDegrees > 0.0f) this.currentRotateDegrees else this.currentRotateDegrees + 360.0f
-                            this.refresh()
+                        TOUCH_STATE_ROTATE -> {
+                            onMapViewRotate(event)
                         }
-                        4 -> {
+                        TOUCH_STATE_TWO_POINTED -> {
                             this.oldDist = this.distance(event, this.mid)
-                            this.currentTouchState = 2
+                            this.currentTouchState = TOUCH_STATE_SCALE
                         }
                     }
                 }
-                5 -> if (event.pointerCount == 2) {
+                MotionEvent.ACTION_POINTER_DOWN -> if (event.pointerCount == 2) {
                     this.saveMatrix.set(this.currentMatrix)
                     this.saveZoom = this.currentZoom
                     this.saveRotateDegrees = this.currentRotateDegrees
                     this.startTouch.set(event.getX(0), event.getY(0))
-                    this.currentTouchState = 4
+                    this.currentTouchState = TOUCH_STATE_TWO_POINTED
                     this.mid = this.midPoint(event)
                     this.oldDist = this.distance(event, this.mid)
                     this.oldDegree = this.rotation(event, this.mid)
                 }
-                6 -> this.currentTouchState = 0
+                MotionEvent.ACTION_POINTER_UP -> this.currentTouchState = TOUCH_STATE_NO
                 else -> { }
             }
 
@@ -127,11 +160,8 @@ open class ImprovedMapView : MapView {
 
     private fun canDragX(event: MotionEvent) : Boolean {
         val currentX: Float
-        val checkingMatrix: Matrix = Matrix()
-        val newMapWidth = when {
-            Math.round(mapWidth / maxX) >= 2 -> mapWidth / Math.round(mapWidth / maxX)
-            else -> mapWidth
-        }
+        val checkingMatrix = Matrix()
+        val newMapWidth = mapWidth
         checkingMatrix.set(this.saveMatrix)
         checkingMatrix.postTranslate(event.x - this.startTouch.x, event.y - this.startTouch.y)
         val values = FloatArray(9)
@@ -143,7 +173,7 @@ open class ImprovedMapView : MapView {
             else -> -1
         }
 
-        if (currentZoom > minZoomModifier && minZoomModifier > 0f) {
+        if (minZoomModifier > 0f) {
             val mapWidthWithScale = mapWidth * currentZoom
 
             return when (direction) {
@@ -160,11 +190,8 @@ open class ImprovedMapView : MapView {
 
     private fun canDragY(event: MotionEvent) : Boolean {
         val currentY: Float
-        val checkingMatrix: Matrix = Matrix()
-        val newMapHeigth = when {
-            Math.round(mapHeight / maxY) >= 2 -> mapHeight / Math.round(mapHeight / maxY)
-            else -> mapHeight
-        }
+        val checkingMatrix = Matrix()
+        val newMapHeigth = mapHeight
         checkingMatrix.set(this.saveMatrix)
         checkingMatrix.postTranslate(event.x - this.startTouch.x, event.y - this.startTouch.y)
         val values = FloatArray(9)
@@ -176,12 +203,12 @@ open class ImprovedMapView : MapView {
             else -> -1
         }
 
-        if (currentZoom > minZoomModifier && minZoomModifier > 0f) {
+        if (minZoomModifier > 0f) {
             val mapHeightWithScale = mapHeight * currentZoom
 
             return when (direction) {
-                TOP -> currentY > if (mapHeightWithScale < maxY) 0f else (mapHeightWithScale - maxY) * -1
-                else  -> if (mapHeightWithScale > maxY) currentY < 0 else currentY < maxY - mapHeightWithScale
+                TOP -> currentY > 0
+                else  -> currentY < maxY - mapHeightWithScale
             }
         } else {
             return when (direction) {
